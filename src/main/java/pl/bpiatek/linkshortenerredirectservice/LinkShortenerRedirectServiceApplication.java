80d47451abcfc5667008a92463f95b6177efc610
Bartosz Piatek <your.email@example.com>
2025-09-20 17:17:18 +0200

    initial commit

---
+++ src/main/java/pl/bpiatek/linkshortenerredirectservice/LinkShortenerRedirectServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.bpiatek.linkshortenerredirectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerRedirectServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShortenerRedirectServiceApplication.class, args);
+    }
+
+}
